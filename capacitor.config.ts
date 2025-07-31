3ab904675dfecbad1ebc543b2e682fdaf027062b
theogaditya <theogadityahota@gmail.com>
2025-07-31 17:19:32 +0530

    android-support

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'portfolio',
+  webDir: 'public',
+   server: {
+    url: 'https://www.adityahota.online/', 
+  }
+};
+
+export default config;
